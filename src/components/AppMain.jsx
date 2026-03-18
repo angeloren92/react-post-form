37956aa236ccb61a3430db8a2b342828817f3eea
angeloren92 <angeloren92@gmail.com>
2026-03-18 15:14:29 +0100

    add form usestate and functions

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,26 +1,45 @@
+import { useState } from "react"
+
 function AppMain() {
+
+  const [formInput, setFormInput] = useState({
+    author: '',
+    title: '',
+    body: '',
+    gridCheck: false
+  })
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+  }
+
+  function handleInputData(e) {
+    const inputValue = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    setFormInput({...formInput, [e.target.name] : inputValue})
+    console.log(formInput)
+  }
 
   return (
     <main>
       <div className="container">
-        <form className="row g-3">
+        <form className="row g-3" onSubmit={handleFormSubmit}>
           <div className="col-12">
-            <label for="author" className="form-label">Autore</label>
-            <input type="text" className="form-control" id="author" name="author" placeholder="Nome Autore" />
+            <label htmlFor="author" className="form-label">Autore</label>
+            <input type="text" className="form-control" id="author" name="author" placeholder="Nome Autore" onChange={handleInputData} value={formInput.author}/>
           </div>
           <div className="col-12">
-            <label for="title" className="form-label">Titolo</label>
-            <input type="text" className="form-control" id="title" name="title" placeholder="Titolo Post" />
+            <label htmlFor="title" className="form-label">Titolo</label>
+            <input type="text" className="form-control" id="title" name="title" placeholder="Titolo Post" onChange={handleInputData} value={formInput.title}/>
           </div>
           <div className="col-12">
-            <label for="body" className="form-label">Testo</label>
-            <textarea type="text" className="form-control" id="body" name="body" />
+            <label htmlFor="body" className="form-label">Testo</label>
+            <textarea type="text" className="form-control" id="body" name="body" onChange={handleInputData} value={formInput.body}/>
           </div>
           <div className="col-12">
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="gridCheck" />
-              <label className="form-check-label" for="gridCheck">
-                Check me out
+              <input className="form-check-input" type="checkbox" id="gridCheck" name="gridCheck" onChange={handleInputData} checked={formInput.gridCheck}/>
+              <label className="form-check-label" htmlFor="gridCheck">
+                Post Pubblico
               </label>
             </div>
           </div>
