242898a58586d27c2c31a0c751daaf4848c6284a
angeloren92 <angeloren92@gmail.com>
2026-03-18 19:19:14 +0100

    add buttons still to fix

--- src/components/AppCards.jsx
+++ src/components/AppCards.jsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 export default function AppCards({ apiUrl }) {
 
     const [data, setData] = useState([])
+    const [pages, setPages] = useState([0, 20])
+    const reversedData = [...data].reverse()
+    const currentPage = reversedData.slice(pages[0], pages[1])
 
     useEffect(() => {
         axios.get(apiUrl)
@@ -14,37 +17,50 @@ export default function AppCards({ apiUrl }) {
             .catch(function (error) {
                 console.log('errore caricamento')
             })
-    }, [])
+    }, [apiUrl])
 
-    const reversedData = [...data].reverse().map(element => element).slice(0, 25)
+    function setNextPages() {
+        setPages([pages[0] +20, pages[1] + 20])
+    }
+    function setPrevPages() {
+        setPages([pages[0] -20, pages[1] - 20])
+    }
+
+    console.log(pages)
 
     return (
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 py-5">
-            {
-                reversedData.map(element => {
-                    return (
-                        <div key={element.id} className="col g-3">
-                            <div className="card h-100">
-                                <div className="card-body d-flex flex-column justify-content-start">
-                                    <div className="card-header">
-                                        <h2 className="h4 mb-0">
-                                            {element.title}
-                                        </h2>
-                                        <small className="text-muted">
-                                            {element.author}
-                                        </small>
-                                    </div>
-                                    <div className="text-center">
-                                        <p className="mt-3">
-                                            {element.body}
-                                        </p>
+        <>
+            <div className="mt-5 d-flex justify-content-between">
+                <button onClick={setPrevPages} className="btn btn-warning" disabled={pages[0] <= 0}>PREV</button>
+                <button onClick={setNextPages} className="btn btn-warning" disabled={pages[1] >= data.length}>NEXT</button>
+            </div>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 py-3">
+                {
+                    currentPage.map(element => {
+                        return (
+                            <div key={element.id} className="col g-3">
+                                <div className="card h-100">
+                                    <div className="card-body d-flex flex-column justify-content-start">
+                                        <div className="card-header">
+                                            <h2 className="h4 mb-0">
+                                                {element.title}
+                                            </h2>
+                                            <small className="text-muted">
+                                                {element.author}
+                                            </small>
+                                        </div>
+                                        <div className="text-center">
+                                            <p className="mt-3">
+                                                {element.body}
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
+                        )
+                    })
+                }
+            </div>
+        </>
     )
 }
